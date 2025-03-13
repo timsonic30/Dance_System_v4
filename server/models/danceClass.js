c92753b22564f9fa17775cbbf0fe2d04ce96860b
LibrasEdna <127267215+LibrasEdna@users.noreply.github.com>
2025-03-13 17:41:11 +0800

    Add files via upload

--- server/models/danceClass.js
+++ server/models/danceClass.js
@@ -16,22 +16,7 @@ const danceClassSchema = new Schema({
   },
   danceStyle: {
     type: String,
-    enum: [
-      "Heels Dance",
-      "Chair Dance",
-      "Jazz Funk",
-      "Twerk",
-      "Hip Hop",
-      "House",
-      "Poping",
-      "Locking",
-      "Girls Hip Hop",
-      "Urban",
-      "Conternporary",
-      "Waacking",
-      "K-pop",
-      "Breaking",
-    ],
+    enum: ["Heels Dance", "Chair Dance", "Jazz Funk", "Twerk", "Hip Hop", "House", "Poping", "Locking", "Girls Hip Hop", "Urban", 'Conternporary', 'Waacking', 'K-pop', 'Breaking'],
   },
   teacher: String,
   numberOfStudent: String,
@@ -44,7 +29,7 @@ const danceClassSchema = new Schema({
   lesson_duration: [{ type: Date }],
   room: { type: String, enum: ["Room X", "Room L", "Room XL"] },
   performanceDay: { type: Date },
-  img: { type: String },
+  img:{ type: String },
   createdAt: { type: Date, default: Date.now },
 });
 const DanceClass = mongoose.model("DanceClass", danceClassSchema);
@@ -52,23 +37,27 @@ const DanceClass = mongoose.model("DanceClass", danceClassSchema);
 module.exports = DanceClass;
 
 //save到mongodb的範例
-const newDanceClass = new DanceClass({
-  classCode: "HS002",
-  classType: "Pop Up Class",
-  danceStyle: "House",
-  teacher: "Liam",
-  numberOfStudent: "12",
-  level: "Open style",
-  date: new Date("2025-04-05"),
-  startTime: "19:00",
-  endTime: "20:30",
-  description: "體驗 House 的輕快節奏與腳步律動。",
-  price: 2200,
-  lesson_duration: [new Date("2025-04-05"), new Date("2025-04-20")],
-  room: "Room L",
-  performanceDay: new Date("2025-04-20"),
-  img: "https://cdn.midjourney.com/5433ed61-1712-41a4-bbc7-709eecbe9bb8/0_3.png",
-});
+// const newDanceClass = new DanceClass({  
+  
+//   classCode: "HS002",
+//     classType: "Pop Up Class",
+//     danceStyle: "House",
+//     teacher: "Liam",
+//     numberOfStudent: "12",
+//     level: "Open style",
+//     date: new Date("2025-04-05"),
+//     startTime: "19:00",
+//     endTime: "20:30",
+//     description: "體驗 House 的輕快節奏與腳步律動。",
+//     price: 2200,
+//     lesson_duration: [new Date("2025-04-05"), new Date("2025-04-20")],
+//     room: "Room L",
+//     performanceDay: new Date("2025-04-20"),
+//     img: "https://cdn.midjourney.com/5433ed61-1712-41a4-bbc7-709eecbe9bb8/0_3.png",
+  
+
+
+// });
 // newDanceClass
 //   .save()
 //   .then((savedDoc) => {
